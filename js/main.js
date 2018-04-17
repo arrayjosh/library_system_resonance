44d7b83828667c81db4507c215dd1364894b47fd
Josh Sanderlin <jsanderlin@arrayschool.com>
2018-04-17 15:35:39 -0600

    added borrowers

--- js/main.js
+++ js/main.js
@@ -4,7 +4,9 @@ const libraryID = 155;
 const req = new Requests(libraryID);
 
 let bookTemplate = $('#templates .bookRow');
+let borrowerTemplate = $('#templates .borrowerRow');
 let bookTable = $('#bookTableBody');
+let borrowerTable = $('#borrowerTableBody');
 
 async function getLibraryName() {
   let library = await req.getLibrary();
@@ -14,14 +16,7 @@ async function getLibraryName() {
 getLibraryName();
 
 
-// let book = {
-//   id: 456,
-//   title: "My Book",
-//   description: "The books description",
-//   image_url: "https://someimageurl",
-//   borrower_id: 25
-//   This sentence is false.
-// }
+
 function addBookToPage(book) {
   let newBook = bookTemplate.clone(true, true);
   newBook.attr('data-id', book.id);
@@ -29,6 +24,14 @@ function addBookToPage(book) {
   newBook.find('.bookTitle').text(book.title);
   newBook.find('.bookDesc').text(book.description);
   bookTable.append(newBook);
+}
+
+function addBorrowerToPage(borrower) {
+  let newBorrower = borrowerTemplate.clone(true, true);
+  newBorrower.attr('data-id', borrower.id);
+  newBorrower.find('.borrowerFirstname').text(borrower.firstname);
+  newBorrower.find('.borrowerLastname').text(borrower.lastname);
+  borrowerTable.append(newBorrower);
 }
 
 async function getBooks() {
@@ -40,7 +43,99 @@ async function getBooks() {
 
 getBooks();
 
-//:P
+async function getBorrowers() {
+  let borrowers = await req.getBorrowers();
+  borrowers.forEach((borrower) => {
+    addBorrowerToPage(borrower);
+  });
+}
+getBorrowers();
+
+
+
+
+
+async function deleteBook(bookRow) {
+  let bookID = bookRow.attr('data-id');
+  await req.deleteBook({id: bookID});
+  bookRow.fadeOut(300, () => bookRow.remove());
+}
+
+async function deleteBorrower(borrowerRow) {
+  let borrowerID = borrowerRow.attr('data-id');
+  await req.deleteBorrower({id: borrowerID});
+  borrowerRow.fadeOut(300, () => borrowerRow.remove());
+}
+
+function deleteAllBooks() {
+  $('.bookRow').each(function() {
+    deleteBook($(this));
+  });
+}
+
+function deleteAllBorrowers() {
+  $('.borrowerRow').each(function() {
+    deleteBorrower($(this));
+  });
+}
+
+$('.deleteBook').click(function() {
+  let bookRow = $(this).parents('.bookRow');
+  deleteBook(bookRow);
+});
+
+$('.deleteBorrower').click(function() {
+  let borrowerRow = $(this).parents('.borrowerRow');
+  deleteBorrower(borrowerRow);
+});
+
+
+$('#addBookForm').on('submit', async function(event) {
+  event.preventDefault();
+
+  // Grab the book form data and create a new book object
+  let newBook = {
+    title: event.target.addBookTitle.value,
+    description: event.target.addBookDescription.value,
+    image_url: event.target.addBookImageURL.value,
+  };
+
+  // Create the book on the server
+  newBook = await req.createBook(newBook);
+
+  // Add the book to our books table
+  addBookToPage(newBook);
+
+  // Reset the form fields
+  event.target.reset();
+
+  // Finally, close the #addBookModal
+  $('#addBookModal').modal('hide');
+
+});
+
+$('#addBorrowerForm').on('submit', async function(event) {
+  event.preventDefault();
+
+  // Grab the book form data and create a new book object
+  let newBorrower = {
+    firstname: event.target.addBorrowerFirstname.value,
+    lastname: event.target.addBorrowerLastname.value,
+  };
+
+  // Create the book on the server
+  newBorrower = await req.createBorrower(newBorrower);
+
+  // Add the book to our books table
+  addBorrowerToPage(newBorrower);
+
+  // Reset the form fields
+  event.target.reset();
+
+  // Finally, close the #addBookModal
+  $('#addBorrowerModal').modal('hide');
+
+});
 
 
 async function testAPI() {
@@ -52,12 +147,12 @@ async function testAPI() {
     image_url: "https://doesntmatter.org"
   });
 
-  console.log(book1);
+
 
   let books = await req.getBooks();
 
 
-  console.log(books);
+
 }
 
 //Has anyone really been far as decided to use even go want to do look more like?
